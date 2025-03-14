2415db93e639f0f4d7722ab976ec9f98a5693389
mbaisotti99 <matteo.baisotti@gmail.com>
2025-03-14 22:55:53 +0100

    style

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $decodedArr = json_decode($discArr, true);
 
 <body>
     <header class="d-fex">
-        <img class="logo" width="100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="logo">
+        <img class="logo" width="70" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="logo">
     </header>
     <div class="container">
         <h1 class="text-center my-5">I tuoi Dischi </h1>
